9c290051e3b395c2c2da9b405dde50004c41571c
Kefa698 <kefaombongi07@gmail.com>
2022-05-28 15:02:12 +0300

    services
    Signed-off-by: Kefa698 <kefaombongi07@gmail.com>

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import styles from '../styles/Services.module.css'
+import React from 'react';
+import styles from '../styles/Services.module.css';
+import Link from 'next/link';
+import Image from 'next/image'
 
-function Services() {
-  return (
-    <div className={styles.container}>Services</div>
-  )
+function Services({ services }) {
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>What Can We Do ?</h1>
+			<h1 className={styles.subtitle}>Services we can help you with</h1>
+			<div className={styles.services}>
+				{services.map((service) => (
+					<Link passHref key={service.id} href={`/product/${service.name}`}>
+						<div className={styles.service}>
+              <div className={styles.desc}>{service.desc}</div>
+							<span className={styles.cat}>{service.title}</span>
+							<div className={styles.media}>
+								{service.video ? (
+									<video
+										src={`/img/${service.video}`}
+										autoPlay
+										muted
+										loop
+										className={styles.video}
+									/>
+								) : (
+									<Image
+										src={`/img/${service.photo}`}
+										alt=""
+										width="100%"
+										height="100%"
+										layout="responsive"
+										objectFit="cover"
+									/>
+								)}
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default Services
+export default Services;
