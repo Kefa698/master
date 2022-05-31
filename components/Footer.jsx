667ca24271f33ab01bd6026e5e08176abf0175e8
Kefa698 <kefaombongi07@gmail.com>
2022-06-01 01:59:14 +0300

    Footer
    Signed-off-by: Kefa698 <kefaombongi07@gmail.com>

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import styles from '../styles/Footer.module.css'
+import React from 'react';
+import styles from '../styles/Footer.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
-function Footer() {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.cardL}>
+				<h1 className={styles.title}>AV0CAD0 CREATIVES.</h1>
+				<h1 className={styles.linkTitle}>
+					<Link href="/contact" className={styles.link} passHref>
+						<>
+							<span className={styles.linkText}>WORK WITH US</span>
+							<Image
+								src={'/img/link.png'}
+								width="40px"
+								height="40px"
+								alt=""
+							/>
+						</>
+					</Link>
+				</h1>
+			</div>
+			<div className={styles.cardS}>
+				<div className={styles.cardItem}>
+					45 ADAM STREET,
+					<br /> USA
+				</div>
+				<div className={styles.cardItem}>
+					CONTACT@kefaisaboke.com
+					<br /> 111_438_5244
+				</div>
+			</div>
+			<div className={styles.cardS}>
+				<div className={styles.cardItem}>
+					FOLLOW US:
+					<br /> __FB __IN __BE __TW
+				</div>
+				<div className={styles.cardItem}>
+					© 2022 ,
+					<br />
+					ALL RIGHTS RESERVED
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
