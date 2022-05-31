6548d5819efb6499aa16f32d7d55e0c6607e8d1c
Kefa698 <kefaombongi07@gmail.com>
2022-05-31 12:02:23 +0300

    updatde
    Signed-off-by: Kefa698 <kefaombongi07@gmail.com>

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { users } from '../data';
+import styles from '../styles/Testimonials.module.css';
+import Circle from './Circle';
+import Image from 'next/image';
 
 function Testimonials() {
-  return (
-    <div>Testimonials</div>
-  )
+	return (
+		<div className={styles.container}>
+			<Circle backgroundColor="darkblue" top="-70vh" left="0" right="0" />
+			<h1 className={styles.title}>Testimonials</h1>
+			<div className={styles.wrapper}>
+				{users.map((user) => (
+					<div key={user.id} className={styles.card}>
+						<Image src={`/img/${user.logo}`} width="30" height="30" alt="" />
+						<p className={styles.comment}>{user.comment}</p>
+						<div className={styles.person}>
+							<Image
+								src={`/img/${user.avatar}`}
+								objectFit="cover"
+								width="45"
+								height="45"
+								alt=""
+							/>
+							<div className={styles.info}>
+								<span className={styles.username}>{user.name}</span>
+								<span className={styles.jobTitle}>{user.title}</span>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default Testimonials
+export default Testimonials;
